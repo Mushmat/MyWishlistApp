12da05310081e585d65106ec5f9599fd777ec6d7
Mushmat <edu.chirayu2005@gmail.com>
2024-06-28 13:48:16 +0530

    Created a dataclass of how the Wish would look like. Each wish has an ID, a title and a description

--- app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -14,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.example.mywishlistapp.data.Wish
 
 @Composable
 fun HomeView(){
@@ -27,7 +29,8 @@ fun HomeView(){
                 modifier = Modifier.padding(all= 20.dp),
                 contentColor = Color.White,
                 backgroundColor = Color.Black,
-                onClick = {// TODO Add Navigation to add screen
+                onClick = {Toast.makeText(context,"FAButton Clicked",Toast.LENGTH_LONG).show()
+                // TODO Add Navigation to add screen
                 }) {
                     Icon(imageVector = Icons.Default.Add, contentDescription = null)
             }
@@ -39,5 +42,12 @@ fun HomeView(){
             .padding(it)){
 
         }
+    }
+}
+
+@Composable
+fun WishItem(wish: Wish, onClick: () -> Unit){
+    Card {
+
     }
 }
