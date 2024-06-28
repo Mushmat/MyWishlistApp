28b815c02278d640c269826d1660744b7561cd21
Mushmat <edu.chirayu2005@gmail.com>
2024-06-28 13:36:21 +0530

    Added the condition where the NavigationIcon will only be displayed when we are on a second or further screen. It will not be displayed on the home page

--- app/src/main/java/com/example/mywishlistapp/AppBar.kt
+++ app/src/main/java/com/example/mywishlistapp/AppBar.kt
@@ -22,7 +22,7 @@ fun AppBarView(
 )
 {
     val navigationIcon: (@Composable ()-> Unit)? =
-        {
+        if(!title.contains("WishList")){{
             IconButton(onClick = {onBackNavClicked()}) {
                 Icon(
                     imageVector = Icons.AutoMirrored.Filled.ArrowBack,
@@ -30,6 +30,8 @@ fun AppBarView(
                     contentDescription = null
                 )
             }
+        } }else{
+            null
         }
 
 
