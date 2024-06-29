e4bea4f7682c43b3e44884249877f8b2f82d32a0
Mushmat <edu.chirayu2005@gmail.com>
2024-06-29 12:57:17 +0530

    Implementing the ViewModel so that everything can be connected.

--- app/src/main/java/com/example/mywishlistapp/WishViewModel.kt
+++ app/src/main/java/com/example/mywishlistapp/WishViewModel.kt
@@ -4,8 +4,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.mywishlistapp.data.WishRepository
 
-class WishViewModel: ViewModel(){
+class WishViewModel(
+    private val wishRepository: WishRepository
+): ViewModel(){
     var wishTitleState by mutableStateOf("")
     var wishDescriptionState by mutableStateOf("")
 
