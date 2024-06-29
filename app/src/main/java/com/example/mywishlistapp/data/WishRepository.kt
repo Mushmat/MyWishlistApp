2164ec7989c7450349c0154d4dd5dea8732500d8
Mushmat <edu.chirayu2005@gmail.com>
2024-06-29 12:55:39 +0530

    Setted up all the five functions in the repository

--- app/src/main/java/com/example/mywishlistapp/data/WishRepository.kt
+++ app/src/main/java/com/example/mywishlistapp/data/WishRepository.kt
@@ -1,6 +1,25 @@
 package com.example.mywishlistapp.data
 
+import kotlinx.coroutines.flow.Flow
+
+
 class WishRepository(private val wishDao: WishDAO) {
 
-    fun addAWish(wish:Wish)
+    suspend fun addAWish(wish:Wish){
+        wishDao.addAWish(wish)
+    }
+
+    fun getWish(): Flow<List<Wish>> = wishDao.getAllWishes() //It is a suspend function automatically as we are using Flow
+
+    fun getAWishById(id:Long): Flow<Wish> {
+        return wishDao.getAWishById(id)
+    }
+
+    suspend fun updateAWish(wish:Wish){
+        wishDao.updateAWish(wish)
+    }
+
+    suspend fun deleteAWish(wish: Wish){
+        wishDao.deleteAWish(wish)
+    }
 }
