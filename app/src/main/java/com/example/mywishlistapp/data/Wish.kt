f87e1b4001853e1411c52ddbe3f982b4a7e5383b
Mushmat <edu.chirayu2005@gmail.com>
2024-06-29 12:28:01 +0530

    Defined the data class as an Entity and added the primary key of the database.

--- app/src/main/java/com/example/mywishlistapp/data/Wish.kt
+++ app/src/main/java/com/example/mywishlistapp/data/Wish.kt
@@ -1,6 +1,12 @@
 package com.example.mywishlistapp.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "wish-table") //Define the table name in which the wishes will be stored, in this table we have columns that store the data
 data class Wish(
+    //Define the primary key, id is the primary key in our case.
+    @PrimaryKey(autoGenerate = true) //This automatically increments the id everytime a new wish is created.
     val id: Long = 0L,
     val title: String="",
     val description:String=""
