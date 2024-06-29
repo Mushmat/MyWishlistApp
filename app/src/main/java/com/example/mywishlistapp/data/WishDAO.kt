c3e96a0c17555aae9f220413568a1db95b72ab68
Mushmat <edu.chirayu2005@gmail.com>
2024-06-29 12:43:58 +0530

    Added five abstract DAO functions: Insert, Update, Delete, Query all wishes ,and Query a specific wish

--- app/src/main/java/com/example/mywishlistapp/data/WishDAO.kt
+++ app/src/main/java/com/example/mywishlistapp/data/WishDAO.kt
@@ -1,11 +1,30 @@
 package com.example.mywishlistapp.data
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 
 
 //ABSTRACT CLASS IS WHERE THE FUNCTIONS NEED NOT BE IMPLEMENTED.
 @Dao
 abstract class WishDAO {
 
-    abstract fun addAWish(wish: Wish) //IT DOESNT NEED A BODY {}. IT IS AN ABSTRACT FUNCTION.
+    @Insert(onConflict = OnConflictStrategy.IGNORE) //The addAWish function will take care of the insertion.
+    abstract fun addAWish(wishEntity: Wish) //IT DOESNT NEED A BODY {}. IT IS AN ABSTRACT FUNCTION.
+
+    @Query("Select * from `wish-table`") //LOADS ALL WISHES FROM THE WISH TABLE
+    abstract fun getAllWishes(): Flow<List<Wish>>
+
+    @Update
+    abstract fun updateAWish(wishEntity: Wish)
+
+    @Delete
+    abstract fun deleteAWish(wishEntity: Wish)
+
+    @Query("Select * from `wish-table` where id =:id") //Get the wish of that specific id
+    abstract fun getAWishById(id:Long): Flow<Wish>
 }
