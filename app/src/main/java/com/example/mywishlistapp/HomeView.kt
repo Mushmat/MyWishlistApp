d8bfe13420f988c2090cd778aa5a8774ecffc347
Mushmat <edu.chirayu2005@gmail.com>
2024-06-29 14:16:40 +0530

    Added background to swipe to delete.

--- app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -1,7 +1,10 @@
 package com.example.mywishlistapp
 
 import android.widget.Toast
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -21,9 +24,12 @@ import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -73,7 +79,19 @@ fun HomeView(
 
             SwipeToDismiss(
                 state = dismissState,
-                background = {},
+                background = {
+                             val color by animateColorAsState(
+                                 if(dismissState.dismissDirection == DismissDirection.EndToStart) Color.Red else Color.Transparent
+                             ,label = ""
+                             )
+                    val alignment = Alignment.CenterEnd
+                    Box(
+                        Modifier.fillMaxSize().background(color).padding(horizontal = 20.dp),
+                        contentAlignment = alignment
+                    ){
+                        Icon(Icons.Default.Delete,"Delete Icon", tint = Color.White)
+                    }
+                },
                 directions = setOf(DismissDirection.StartToEnd, DismissDirection.EndToStart),
                 dismissThresholds = {FractionalThreshold(0.25f)},
                 dismissContent = {
